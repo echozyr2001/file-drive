ee375ca464b8c659545c78ace644631856586b97
echozyr2001 <echo.zyr.2001@gmail.com>
2024-03-29 21:17:21 +0800

    chore: marketing page
    build marketing page

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,7 +1,15 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
 const HomePage = () => {
   return (
-    <div>
-      <h1>Home Page</h1>
+    <div className="flex-1 w-full flex flex-col items-center justify-center p-4 gap-2">
+      <div className="relative w-[500px] h-[500px] mb-8">
+        <Image src="/empty.svg" alt="Empty" fill />
+      </div>
+      <div>
+        <Button>Upload</Button>
+      </div>
     </div>
   );
 };
