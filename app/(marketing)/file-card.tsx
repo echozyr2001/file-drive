fffa1ef5fdc811de4e68e9e3148db6c4d786de31
echozyr2001 <echo.zyr.2001@gmail.com>
2024-04-05 17:25:35 +0800

    refactor: card display
    add card compoent to display file

---
+++ app/(marketing)/file-card.tsx
@@ -0,0 +1,26 @@
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+export const FileCard = () => {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Card Title</CardTitle>
+        <CardDescription>Card Description</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <p>Card Content</p>
+      </CardContent>
+      <CardFooter>
+        <Button>Download</Button>
+      </CardFooter>
+    </Card>
+  );
+};
--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -4,6 +4,7 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import Image from "next/image";
 import { UploadDialog } from "./upload-dialog";
+import { FileCard } from "./file-card";
 
 const HomePage = () => {
   return (
@@ -17,6 +18,16 @@ const HomePage = () => {
           </Button>
         </div>
         <UploadDialog />
+      </div>
+      <div className=" grid grid-cols-4 gap-4">
+        <FileCard></FileCard>
+        <FileCard></FileCard>
+        <FileCard></FileCard>
+        <FileCard></FileCard>
+        <FileCard></FileCard>
+        <FileCard></FileCard>
+        <FileCard></FileCard>
+        <FileCard></FileCard>
       </div>
       <div className="w-full h-full flex flex-col items-center justify-center p-4 gap-4">
         {/* only use priority for importamt image */}
