3550e0c9a95d3411c280c29415f7862b0d81f569
echozyr2001 <echo.zyr.2001@gmail.com>
2024-04-08 20:05:34 +0800

    perf: rebuild marketing page
    rebuild marketing page

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -8,6 +8,7 @@ import { FileCard } from "./file-card";
 import { useQuery } from "convex/react";
 import { api } from "@/convex/_generated/api";
 import { useOrganization } from "@clerk/nextjs";
+import { Loader2 } from "lucide-react";
 
 const HomePage = () => {
   // TODO: fix this organization may be null
@@ -30,24 +31,30 @@ const HomePage = () => {
         <UploadDialog />
       </div>
       <div className="h-full">
-        {/* only use priority for importamt image */}
-        {!files || files.length === 0 ? (
-          <div className="flex flex-col items-center justify-center h-full gap-8">
-            <Image
-              src="/empty.svg"
-              alt="Empty"
-              width={0}
-              height={0}
-              priority
-              className="w-[500px] h-auto"
-            />
-            <div className="text-2xl">You have no files, go to upload now</div>
-          </div>
+        {files ? (
+          files.length > 0 ? (
+            <div className="grid grid-cols-4 gap-4 px-8">
+              {files.map((file) => (
+                <FileCard key={file._id} file={file} />
+              ))}
+            </div>
+          ) : (
+            <div className="flex flex-col items-center justify-center h-full gap-8">
+              <Image
+                src="/empty.svg"
+                alt="Empty"
+                priority
+                className="w-[500px] h-auto"
+              />
+              <div className="text-2xl">
+                You have no files, go to upload now
+              </div>
+            </div>
+          )
         ) : (
-          <div className="grid grid-cols-4 gap-4 px-8">
-            {files?.map((file) => (
-              <FileCard key={file._id} file={file} />
-            ))}
+          <div className="flex flex-col items-center justify-center h-full gap-4">
+            <Loader2 className="h-52 w-52 animate-spin" />
+            <div className="text-2xl">Loading...</div>
           </div>
         )}
       </div>
